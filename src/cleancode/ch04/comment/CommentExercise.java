bc41f86f3bce978d8c961e29b76fcb29bf541212
cynicjj <cynicjj@DESKTOP-IR5AFJB>
2023-10-26 19:24:54 +0900

    4장 주석 실습 클래스 CommentExercise 추가

---
+++ src/cleancode/ch04/comment/CommentExercise.java
@@ -0,0 +1,18 @@
+package cleancode.ch04.comment;
+
+public class CommentExercise {
+
+	void removeComment() {
+
+		// 직원이 복지 혜택을 받을 자격이 있는지 검사
+		if ((employee.flags & HOURLY_FLAG) && (employee.age > 65)) {
+
+		}
+
+		assertTrue(a.compareTo(b) == -1); // a < b
+	}
+
+	// 테스트 중인 Responder 인스턴스를 반환한다
+	protected abstract Responder responderInstance();
+
+}
