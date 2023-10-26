a7968f928b9a971c783f4bf9e834e93915d87c4a
cynicjj <cynicjj@DESKTOP-IR5AFJB>
2023-10-26 19:11:56 +0900

    3장 실습 추가

--- src/cleancode/ch03/function/exercise/UserValidator.java
+++ src/cleancode/ch03/function/exercise/UserValidator.java
@@ -16,4 +16,25 @@ public class UserValidator {
 		}
 		return false;
 	}
+
+	public void deleteKey() {
+		if (deletePage(page) == E_OK) {
+			if (registry.deleteReference(page.name) == E_OK) {
+				if (configKeys.deleteKey(page.name.makeKey()) == E_OK) {
+					logger.log("page deleted");
+				} else {
+					logger.log("configKey not deleted");
+				}
+			} else {
+				logger.log("deleteReference from registry failed");
+			}
+		} else {
+			logger.log("delete failed");
+			return E_ERROR;
+		}
+	}
+	
+	public void render(boolean isSuite) {
+		
+	}
 }
